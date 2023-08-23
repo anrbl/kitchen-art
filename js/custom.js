b56ffbd019271b8c7e029d548030587a19058b0b
anrbl <eujin0903@gmail.com>
2023-08-23 18:16:44 +0900

    2일차

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,20 @@ $(function () {
         sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
     });
 
+
     $('.main_slide').slick({
         arrows: false,
         fade: true,
         autoplay: true,
         autoplaySpeed: 4000,
+        pauseOnFocus: false,
     });
+    $('.arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    })
+    $('.arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    })
 
     $('.brand_set>li').on('click', function (e) {
         e.preventDefault();
@@ -18,7 +26,15 @@ $(function () {
         $('.brand_story>li').eq(idx).addClass('on').siblings().removeClass('on');
     });
 
-    $('.review_slide').slick({
-        arrows: false
+    $(window).scroll(function () {
+        let top = $(window).scrollTop();
+        top > 100 ? $('.top').fadeIn() : $('.top').fadeOut(0)
+    });
+    $('.top').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 600);
+    })
+
+    $('.mopen').on('click', function () {
+        $(this).toggleClass('on')
     });
 });
